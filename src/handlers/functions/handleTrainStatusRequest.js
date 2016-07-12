6d6fa3e8c048cd56da3043509196bc760335cd39
Prince Raju <princerajukl@gmail.com>
2016-07-12 15:09:28 +0530

    Trial on train enquiry

--- src/handlers/functions/handleTrainStatusRequest.js
+++ src/handlers/functions/handleTrainStatusRequest.js
@@ -1,14 +1,15 @@
 'use strict';
 
 var config=require('../../configs');
+var railways=require('../../services/railwayapi/apiParser');
 
 exports.handleTrainStatusRequest=function(intent, session, response) {
     // Get a random space fact from the space facts list
     
-
+    var result_string=railways.getJsonLiveStatus('12046','20160712');
     // Create speech output
-    var speechOutput = "Here's your train details fact: " + "put details here";
+    var speechOutput = "The correct train name recieved: " + intent.slots.Train.value;
 
-    response.tellWithCard(speechOutput, "Raily- Indian Railways", speechOutput);
+    response.tellWithCard(speechOutput, result_string , speechOutput);
 };
 
