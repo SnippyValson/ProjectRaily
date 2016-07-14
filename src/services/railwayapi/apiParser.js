e768e8bdb0129f817661017488543a09b50e1ca9
Snippy Valson <v.snippy@gmail.com>
2016-07-14 15:50:32 +0530

    Changed the PNR not generated response. -Snippy

--- src/services/railwayapi/apiParser.js
+++ src/services/railwayapi/apiParser.js
@@ -241,7 +241,7 @@ exports.getJsonPNRstatus=function (pnr_no, eventCallback){
                result=result+" Booking status "+booking_status[j]+", Current status "+current_status[j]+", Coach position "+coach_position[j]+".";
                }
                 if(stringResult.response_code=='410')
-                    result="PNR not yet generated.";
+                    result="PNR does not exist.";
             var result1={speech:result,status:result,heading:"PNR status of: "+pnr_no};
             eventCallback(result1);          
               
