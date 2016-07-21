54eee2f1d4214c4a2a0a837653adca7f0964da5e
Prince Raju <princerajukl@gmail.com>
2016-07-21 11:24:35 +0530

    Removed repeated custom slot entry

--- src/handlers/functions/handleTrainArrivalsRequest.js
+++ src/handlers/functions/handleTrainArrivalsRequest.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var config=require('../../configs');
+var stationHere=require('../../stations');
 var railways=require('../../services/railwayapi/apiParser');
 
 exports.handleTrainArrivalsRequest=function(intent, session, response) {
@@ -9,19 +10,29 @@ exports.handleTrainArrivalsRequest=function(intent, session, response) {
 	var station=intent.slots.Station.value;
 	var durationHours=intent.slots.duration.value;
 
-	railways.getJsonTrainArrivals(station,durationHours, function (events){
-		// Create speech output
-		var speechOutput =  events;
+	var stationCode=stationHere.getStationCode(station);
 
-		if(speechOutput['heading']!=null)
-		{
-			response.tellWithCardSSML(speechOutput['speech'], speechOutput['heading'] , speechOutput['status']);
-		}
-		else
-		{
-			response.ask(speechOutput['speech']);
-		}
-	});
+	if(stationCode!=-1)
+	{
+
+		railways.getJsonTrainArrivals(stationCode,durationHours, function (events){
+			// Create speech output
+			var speechOutput =  events;
+
+			if(speechOutput['heading']!=null)
+			{
+				response.tellWithCardSSML(speechOutput['speech'], speechOutput['heading'] , speechOutput['status']);
+			}
+			else
+			{
+				response.ask(speechOutput['speech']);
+			}
+		});
+	}
+	else
+	{
+		response.ask('Station not suported or Incorrect station name');
+	}
 
 
 };
