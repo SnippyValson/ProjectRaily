aad1718adddf4567cb3eb1bd328810bcc0ec2c37
Snippy Valson <v.snippy@gmail.com>
2016-07-21 10:05:32 +0530

    Added stations.js for storing station codes and names for custom slots.

---
+++ src/stations.js
@@ -0,0 +1,61 @@
+/**
+ * Created by Snippy Valson on 21/7/16.
+ */
+var stations=[
+    {
+        "name":"Alappuzha",
+        "code":"ALLP"
+    },
+    {
+        "name":"Aluva",
+        "code":"AWY"
+    },
+    {
+        "name":"Angamaly",
+        "code":"AFK"
+    },
+    {
+        "name":"Badagara",
+        "code":"BDJ"
+    },
+    {
+        "name":"Bekal Fort",
+        "code":"BFR"
+    },
+    {
+        "name":"Calicut",
+        "code":"CLT"
+    },
+    {
+        "name":"Cannanore",
+        "code":"CAN"
+    },
+    {
+        "name":"Chalakudi",
+        "code":"CKI"
+    },
+    {
+        "name":"Changanacherry",
+        "code":"CGY"
+    },
+    {
+        "name":"Charvattur",
+        "code":"CHV"
+    },
+    {
+        "name":"Chengannur",
+        "code":"CNGR"
+    },
+    {
+        "name":"Chirayinkil",
+        "code":"CRY"
+    },
+    {
+        "name":"Dhanuvachapuram",
+        "code":"DAVM"
+    }
+
+
+
+
+];
