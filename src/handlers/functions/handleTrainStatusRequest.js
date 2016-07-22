6b7bd174be5abddc24bb3f95cf8ba3f5d7c96f2c
SRUTHI VARGHESE <sruthivarghese15@gmail.com>
2016-07-22 09:54:18 +0530

    minor bugfix on date

--- src/handlers/functions/handleTrainStatusRequest.js
+++ src/handlers/functions/handleTrainStatusRequest.js
@@ -8,52 +8,18 @@ exports.handleTrainStatusRequest=function(intent, session, response, type) {
     //intent.slots.Train.value;
     // type= "number" / "name"
     var date_ = new Date().getDate();
-    switch(date_)
-    {
-        case 1:date_='01';
-            break;
-        case 2:date_='02';
-            break;
-        case 3:date_='03';
-            break;
-        case 4:date_='04';
-            break;
-        case 5:date_='05';
-            break;
-        case 6:date_='06';
-            break;
-        case 7:date_='07';
-            break;
-        case 8:date_='08';
-            break;
-        case 9:date_='09';
-            break;
-
-    }
     var month_ = new Date().getMonth();
     month_= month_+1;
-    switch(month_)
-    {
-        case 1:month_='01';
-            break;
-        case 2:month_='02';
-            break;
-        case 3:month_='03';
-            break;
-        case 4:month_='04';
-            break;
-        case 5:month_='05';
-            break;
-        case 6:month_='06';
-            break;
-        case 7:month_='07';
-            break;
-        case 8:month_='08';
-            break;
-        case 9:month_='09';
-            break;
+    if(date_<10)
+        {  
+         	date_='0'+date_;
+	}
 
-    }
+  if(month_<10) 
+        {
+	       month_='0'+month_;
+        }
+   
     var year_ = new Date().getFullYear();
     var result = "";
     result= result + year_+month_+date_;
